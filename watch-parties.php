d165086ff5b70f2fae9ef51ee1054382d01054e3
codysilverglat <c.silverglat@gmail.com>
2020-09-21 20:44:37 -0500

    add images for watch-parties, contact pages

--- watch-parties.php
+++ watch-parties.php
@@ -8,11 +8,11 @@
 
 <main>
   <section class="copy">
-  <p><span>Saturdays in the fall...</span></p>
-  <p></p>
+    <p><span>Copy header</span></p>
+    <p></p>
   </section>
   <section class="image">
-  <img src="<?php echo get_template_directory_uri() ?>/assets/chase-daniel.png" />
+    <img src="<?php echo get_template_directory_uri() ?>/assets/watch-party.png" />
   </section>
 </main>
 
